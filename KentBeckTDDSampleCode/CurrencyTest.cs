5f9d07376cc64b4796d082f529d41ab8144d7539
Gary Yang <garyyang0236@gmail.com>
2022-03-10 02:27:51 +0800

    [Chepter 1] Red light

--- KentBeckTDDSampleCode/CurrencyTest.cs
+++ KentBeckTDDSampleCode/CurrencyTest.cs
@@ -1,25 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using NUnit.Framework;
 
 namespace KentBeckTDDSampleCode
 {
-    [TestFixture]
-    public class CurrencyTest
-    {
-        [Test]
-        public void Test_Multiplication()
-        {
-            Dollar five = new Dollar(5);
-            five.times(2);
-            Assert.Equals(10, five.amount);
-        }
-    }
-
     public class Dollar
     {
+        public decimal Amount;
+
         public Dollar(int i)
+        {
+        }
+
+        public void times(int i)
         {
         }
     }
---
+++ KentBeckTDDSampleCodeTest/CurrencyTest.cs
@@ -0,0 +1,17 @@
+﻿using KentBeckTDDSampleCode;
+using NUnit.Framework;
+
+namespace KentBeckTDDSampleCodeTest
+{
+    [TestFixture]
+    public class CurrencyTest
+    {
+        [Test]
+        public void Test_Multiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.Equals(10, five.Amount);
+        }
+    }
+}

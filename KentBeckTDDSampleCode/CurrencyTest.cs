f56fb225bb6de17417ee7522fe7222e72f951ab4
Gary Yang <garyyang0236@gmail.com>
2022-03-10 02:12:50 +0800

    [Chapter 1] Init testMultiplication test

---
+++ KentBeckTDDSampleCode/CurrencyTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace KentBeckTDDSampleCode
+{
+    [TestFixture]
+    public class CurrencyTest
+    {
+        [Test]
+        public void Test_Multiplication()
+        {
+            Dollar five = new Dollar(5);
+            five.times(2);
+            Assert.Equals(10, five.amount);
+        }
+    }
+}

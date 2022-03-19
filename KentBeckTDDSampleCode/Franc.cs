a85264d3cddba129b6012470ad58d03483b7d71d
Gary Yang <garyyang0236@gmail.com>
2022-03-20 01:34:00 +0800

    [Chapter 6] Franc inherit Money

--- KentBeckTDDSampleCode/Franc.cs
+++ KentBeckTDDSampleCode/Franc.cs
@@ -1,23 +1,22 @@
 ﻿namespace KentBeckTDDSampleCode
 {
-    public class Franc
+    public class Franc : Money
     {
-        private readonly decimal _amount;
 
         public Franc(decimal amount)
         {
-            _amount = amount;
+            Amount = amount;
         }
 
         public Franc Times(int multiplier)
         {
-            return new Franc(_amount * multiplier);
+            return new Franc(Amount * multiplier);
         }
 
         public override bool Equals(object? obj)
         {
             var franc = (Franc)obj;
-            return franc._amount == _amount;
+            return franc.Amount == Amount;
         }
     }
 }

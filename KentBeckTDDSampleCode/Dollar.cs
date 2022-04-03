af316c2afd759392fa6a5af342ce8f229b231d02
Gary Yang <garyyang0236@gmail.com>
2022-04-04 00:23:13 +0800

    [Chapter 10] Inline factory method

--- KentBeckTDDSampleCode/Dollar.cs
+++ KentBeckTDDSampleCode/Dollar.cs
@@ -8,7 +8,7 @@
 
         public override Money Times(int multiplier)
         {
-            return Dollar(Amount * multiplier);
+            return new Dollar(Amount * multiplier, "USD");
         }
     }
 }
